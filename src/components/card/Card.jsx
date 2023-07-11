660c9dfbedc9772877e465dffbc3208e9dcd16ca
ozlem <ozlemimsares@gmail.comgit config --global user.email ozlemimsares@gmail.comgit config --global user.email ozlemimsares@gmail.com>
2023-07-11 21:20:42 +0300

    first

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+// import resim1 from "../helper/resim1.jpg";
+// import resim2 from "../helper/resim2.jpg";
+// import resim3 from "../helper/resim3.jpg";
+// import CardSlider from "./CardSlider";
+// import UniName from "./UniName";
+// import CardLocation from "./CardLocation";
+import CardPre from "./CardPre";
+const Card = () => {
+  return (
+    <div className="card flex flex-wrap  justify-between w-10/12 mx-auto mt-12 mb-10 ">
+      <div className="flex gap-20 mx-auto mb-10">
+        <CardPre />
+        <CardPre />
+        <CardPre />
+      </div>
+      <div className="flex gap-20 mx-auto">
+        <CardPre />
+        <CardPre />
+        <CardPre />
+      </div>
+
+      {/* <div className="flex flex-col-3 justify-evenly w-96 mx-auto space-x-10 "></div> */}
+    </div>
+  );
+};
+export default Card;
