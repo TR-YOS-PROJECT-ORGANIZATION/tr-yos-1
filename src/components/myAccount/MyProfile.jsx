6fecce2455490cd27cd133e5a77db865b603618e
Banu Güngör <banuuzun9@gmail.com>
2023-07-11 23:28:29 +0300

    second

---
+++ src/components/myAccount/MyProfile.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import RightMyAccount from "./RightMyAccount";
+import RightSocialAccounts from "./RightSocialAccounts";
+import RightSaveButton from "./RightSaveButton";
+
+const MyProfile = () => {
+  return (
+    <>
+      <div className="border-2 rounded-lg w-full mb-16 shadow-md shadow-grey-primary ">
+        <div>
+          <p className="text-2xl font-bold p-5">My Account</p>
+          <RightMyAccount />
+          <RightSocialAccounts />
+          <RightSaveButton />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default MyProfile;
