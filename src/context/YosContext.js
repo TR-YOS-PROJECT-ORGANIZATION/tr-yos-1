2e36e8a6ae8dccc86517139ed5a8821c61ac9e65
ibrahim-sert <evo3506@gmail.com>
2023-07-12 23:45:54 +0300

    context changed

--- src/context/YosContext.js
+++ src/context/YosContext.js
@@ -5,6 +5,27 @@ import axios from "axios";
 export const YosContext = createContext();
 
 const YosContextProvider = ({ children }) => {
+    const [location, setLocation] = useState([]);
+    const [uni, setUni] = useState([]);
+    const [depertman, setDepertman] = useState([]);
+    const [city, setCity] = useState([]);
+    const [uniId, setUniId] = useState([]);
+    const [filterDep, setFilterDep] = useState([]);
+  
+    const ApiKey =
+      "mBbAINPS8DwIL5J9isMwnEJGr4OgSkC55SCm2BqnVeJ8r1gxGFlrl8mFN7Q18GA9D/HsXeDS5arTZx6l974b31678f8f18db56809a16f9728baf";
+    const BASE_URL_LOCA = `https://tr-yös.com/api/v1/location/allcities.php?token=${ApiKey}`;
+    const BASE_URL_UNI = `https://tr-yös.com/api/v1/education/alluniversities.php?token=${ApiKey}`;
+    const BASE_URL_DEP = `https://tr-yös.com/api/v1/record/alldepartments.php?token=mBbAINPS8DwIL5J9isMwnEJGr4OgSkC55SCm2BqnVeJ8r1gxGFlrl8mFN7Q18GA9D/HsXeDS5arTZx6l974b31678f8f18db56809a16f9728baf`;
+  
+    const getLoca = async () => {
+      try {
+        const { data } = await axios(BASE_URL_LOCA);
+        setLocation(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
   return <YosContext.Provider value={values}>{children}</YosContext.Provider>;
 };
 
