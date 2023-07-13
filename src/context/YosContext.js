469ab6b9b4ecb4042d070431f55dbb973b20db0c
Banu Güngör <banuuzun9@gmail.com>
2023-07-13 15:16:59 +0300

    hatalar

--- src/context/YosContext.js
+++ src/context/YosContext.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { createContext } from "react";
 // import axios from "axios";
@@ -5,114 +6,116 @@ import { createContext } from "react";
 export const YosContext = createContext();
 
 const YosContextProvider = ({ children }) => {
-    const [location, setLocation] = useState([]);
-    const [uni, setUni] = useState([]);
-    const [depertman, setDepertman] = useState([]);
-    const [city, setCity] = useState([]);
-    const [uniId, setUniId] = useState([]);
-    const [filterDep, setFilterDep] = useState([]);
-  
-    const ApiKey =
-      "mBbAINPS8DwIL5J9isMwnEJGr4OgSkC55SCm2BqnVeJ8r1gxGFlrl8mFN7Q18GA9D/HsXeDS5arTZx6l974b31678f8f18db56809a16f9728baf";
-    const BASE_URL_LOCA = `https://tr-yös.com/api/v1/location/allcities.php?token=${ApiKey}`;
-    const BASE_URL_UNI = `https://tr-yös.com/api/v1/education/alluniversities.php?token=${ApiKey}`;
-    const BASE_URL_DEP = `https://tr-yös.com/api/v1/record/alldepartments.php?token=mBbAINPS8DwIL5J9isMwnEJGr4OgSkC55SCm2BqnVeJ8r1gxGFlrl8mFN7Q18GA9D/HsXeDS5arTZx6l974b31678f8f18db56809a16f9728baf`;
-  
-    const getLoca = async () => {
-      try {
-        const { data } = await fetch(BASE_URL_LOCA);
-        setLocation(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+  const [location, setLocation] = useState([]);
+  const [uni, setUni] = useState([]);
+  const [depertman, setDepertman] = useState([]);
+  const [city, setCity] = useState([]);
+  const [uniId, setUniId] = useState([]);
+  const [filterDep, setFilterDep] = useState([]);
 
-    const getUni = async () => {
-      try {
-        const { data } = await fetch(BASE_URL_UNI);
-        setUni(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-  
-    const getDep = async () => {
-      try {
-        const { data } = await fetch(BASE_URL_DEP);
-        setDepertman(data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    useEffect(() => {
-      getLoca();
-      getUni();
-      getDep();
-    }, []);
-  
-    const getCities = () => {
-      return city?.map((item) => item.value);
-    };
-  
-    const cities = getCities();
-  
-    const getUniId = () => {
-      return uniId?.map((item) => item.value);
-    };
-  
-    const uniIdies = getUniId();
-  
-    const getFilterDep = () => {
-      return filterDep?.map((item) => item.label);
-    };
-    const filterDepss = getFilterDep();
-const options = location?.map((item) => ({
-  value: item.id,
-  label: item.tr,
-}));
+  const ApiKey =
+    "mBbAINPS8DwIL5J9isMwnEJGr4OgSkC55SCm2BqnVeJ8r1gxGFlrl8mFN7Q18GA9D/HsXeDS5arTZx6l974b31678f8f18db56809a16f9728baf";
+  const BASE_URL_LOCA = `https://tr-yös.com/api/v1/location/allcities.php?token=${ApiKey}`;
+  const BASE_URL_UNI = `https://tr-yös.com/api/v1/education/alluniversities.php?token=${ApiKey}`;
+  const BASE_URL_DEP = `https://tr-yös.com/api/v1/record/alldepartments.php?token=mBbAINPS8DwIL5J9isMwnEJGr4OgSkC55SCm2BqnVeJ8r1gxGFlrl8mFN7Q18GA9D/HsXeDS5arTZx6l974b31678f8f18db56809a16f9728baf`;
 
-const options1 = uni
-  ?.filter((item) => cities.includes(item.city))
-  .map((item) => ({
-    value: item.code,
+  const getLoca = async () => {
+    try {
+      const { data } = await axios(BASE_URL_LOCA);
+      setLocation(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getUni = async () => {
+    try {
+      const { data } = await axios(BASE_URL_UNI);
+      setUni(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getDep = async () => {
+    try {
+      const { data } = await axios(BASE_URL_DEP);
+      setDepertman(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    getLoca();
+    getUni();
+    getDep();
+  }, []);
+
+  const getCities = () => {
+    return city?.map((item) => item.value);
+  };
+
+  const cities = getCities();
+
+  const getUniId = () => {
+    return uniId?.map((item) => item.value);
+  };
+
+  const uniIdies = getUniId();
+
+  const getFilterDep = () => {
+    return filterDep?.map((item) => item.label);
+  };
+  const filterDepss = getFilterDep();
+  const options = location?.map((item) => ({
+    value: item.id,
     label: item.tr,
-    img: item.images,
   }));
 
-const options2 = depertman
-  ?.filter((item) => uniIdies.includes(item.university.code))
-  .map((item) => ({
-    value: item.department.code,
-    label: item.department.tr,
-    faculty: item.faculty.tr,
-    university: item.university.tr,
-    address: item.city.tr,
-  }));
+  const options1 = uni
+    ?.filter((item) => cities.includes(item.city))
+    .map((item) => ({
+      value: item.code,
+      label: item.tr,
+      img: item.images,
+    }));
 
-console.log(options2);
-const optionsCard = depertman
-  ?.filter((item) => filterDepss.includes(item.university.code))
-  .map((item) => ({
-    value: item.department.code,
-    label: item.department.tr,
-  }));
+  const options2 = depertman
+    ?.filter((item) => uniIdies.includes(item.university.code))
+    .map((item) => ({
+      value: item.department.code,
+      label: item.department.tr,
+      faculty: item.faculty.tr,
+      university: item.university.tr,
+      address: item.city.tr,
+    }));
 
-const filterrrr = options2?.filter((item) => filterDepss.includes(item.label));
+  console.log(options2);
+  const optionsCard = depertman
+    ?.filter((item) => filterDepss.includes(item.university.code))
+    .map((item) => ({
+      value: item.department.code,
+      label: item.department.tr,
+    }));
 
-const values = {
-  options,
-  options1,
-  options2,
-  setCity,
-  setUniId,
-  setFilterDep,
-  uni,
-  filterDep,
-  uniId,
-  city,
-  optionsCard,
-  filterrrr,
-};
+  const filterrrr = options2?.filter((item) =>
+    filterDepss.includes(item.label)
+  );
+
+  const values = {
+    options,
+    options1,
+    options2,
+    setCity,
+    setUniId,
+    setFilterDep,
+    uni,
+    filterDep,
+    uniId,
+    city,
+    optionsCard,
+    filterrrr,
+  };
   return <YosContext.Provider value={values}>{children}</YosContext.Provider>;
 };
 
