5364bd08aebd4416820da6ff7fce97b7517d4253
Banu Güngör <banuuzun9@gmail.com>
2023-07-19 11:24:00 +0300

    component düzeltmeleri

--- src/components/myAccount/Dashboard.jsx
+++ src/components/myAccount/Dashboard.jsx
@@ -1,18 +1,14 @@
 import React from "react";
-import CardPre from "../CardPre";
+
 
 const Dashboard = () => {
   return (
     <>
       <div className="border-2 rounded-lg w-full mb-16 shadow-md shadow-grey-primary ">
         <div>
-          <p className="text-2xl font-bold p-5">My Favorite Universities</p>
+          <p className="text-2xl font-bold p-5">Dashboard</p>
           <div className="flex flex-wrap m-5 gap-4 justify-evenly">
-            <CardPre />
-            <CardPre />
-
-            <CardPre />
-            <CardPre />
+          
           </div>
         </div>
       </div>
