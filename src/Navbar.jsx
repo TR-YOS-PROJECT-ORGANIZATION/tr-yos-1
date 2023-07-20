04d1105223f196d0f77e08706373b367660673bb
Banu Güngör <banuuzun9@gmail.com>
2023-07-20 12:39:11 +0300

    navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -151,7 +151,7 @@ const Navbar2 = () => {
               </li>
             </ul>
           </div>
-          {loginState.status !== "success" ? (
+          {loginState.status == "success" ? (
             <div>
               <LoginModal />
               <RgisterModal />
@@ -189,7 +189,7 @@ const Navbar2 = () => {
                 <ul className="py-2" aria-labelledby="user-menu-button">
                   <li>
                     <a
-                      href="/myprofile"
+                      href="/"
                       className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                     >
                       Dashboard
